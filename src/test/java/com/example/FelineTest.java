053692c4e6b1c032891989e2c99454b773a795f3
Alexey Sharov <sixthline25@gmail.com>
2023-03-05 23:13:21 +0300

    Написал тесты на класс Feline

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,34 +1,48 @@
 package com.example;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Spy
+    Feline spyFeline;
 
-    @After
-    public void tearDown() throws Exception {
-    }
-
+    // Проверяем что при вызове метода "eatMeat()", метод "getFood()" вызывается с параметром "Хищник"
     @Test
-    public void getFood() {
+    public void eatMeatCallsGetFoodWithCorrectValue() throws Exception {
+        spyFeline.eatMeat();
+        Mockito.verify(spyFeline).getFood("Хищник");
     }
 
+    // Проверяем что метод "getFamily()" возвращает название семейства "Кошачьи"
     @Test
-    public void getFamily() {
+    public void getFamilyReturnsCorrectValue() {
+        Feline feline = new Feline();
+        String expectedResult = "Кошачьи";
+        String actualResult = feline.getFamily();
+        assertEquals(expectedResult, actualResult);
     }
 
+    // Проверяем что при вызове метода "getKittens()", метод "getKittens(int kittensCount)" вызывается с параметром "1"
     @Test
-    public void eatMeat() {
+    public void getKittensCallsGetKittensWithCorrectValue() {
+        spyFeline.getKittens();
+        Mockito.verify(spyFeline).getKittens(1);
     }
 
+    // Проверяем что при вызове метода "getKittens(int kittensCount)", возвращается корректное значение
     @Test
-    public void testGetFamily() {
+    public void getKittensWithKittensCountReturnsCorrectValue() {
+        Feline feline = new Feline();
+        int expectedResult = 5;
+        int actualResult = feline.getKittens(5);
+        assertEquals(expectedResult, actualResult);
     }
 }
